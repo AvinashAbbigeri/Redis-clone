a8ff6cc9dffa4b199c0bd13c5a4ad7df91d098b3
AvinashAbbigeri <avinashabbigeri40@gmail.com>
2025-03-01 12:45:09 +0530

    session2

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module Redis
 
 go 1.23.5
+
+require github.com/tidwall/resp v0.1.1
--- proto_test.go
+++ proto_test.go
@@ -5,22 +5,13 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"testing"
 
 	"github.com/tidwall/resp"
 )
 
-const (
-	CommandSET = "SET"
-)
-
-type Command interface {
-}
-
-type SetCommand struct {
-	key, val string
-}
-
-func parseCommand(raw string) (Command, error) {
+func TestProtocol(t *testing.T) {
+	raw := "*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n"
 	rd := resp.NewReader(bytes.NewBufferString(raw))
 
 	for {
@@ -33,10 +24,13 @@ func parseCommand(raw string) (Command, error) {
 		}
 		fmt.Printf("Read %s\n", v.Type())
 		if v.Type() == resp.Array {
-			for i, v := range v.Array() {
-				fmt.Printf("#%d %s, value: '%s'\n", i, v.Type(), v)
+			for _, v := range v.Array() {
+				switch v.String() {
+				case CommandSET:
+				default:
+					
+				}
 			}
 		}
 	}
-	return "foo", nil
 }
